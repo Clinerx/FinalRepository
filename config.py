17055b0877492e9aa5015fbf3c1de3ace30db72c
Clinerx <chrisdanieldaquioag6@gmail.com>
2023-06-04 18:12:15 +0800

    Debug config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from app import app
 from flaskext.mysql import MySQL
 
-MySQL = MySQL()
+mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'apzx0dfd6'
-app.config['MYSQL_DATABASE_DB'] = 'badminton_reservation'
+app.config['MYSQL_DATABASE_DB'] = 'badminto_reservation'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
