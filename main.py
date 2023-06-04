fcbe74bbe9ed0d1c1a1db393f5d600ea684bdf9e
Clinerx <chrisdanieldaquioag6@gmail.com>
2023-06-04 19:52:31 +0800

    Debug code error in main.py

--- main.py
+++ main.py
@@ -1,111 +1,98 @@
-import pymysql
-from app import app
-from config import mysql
-from flask import jsonify
-from flask import flash, request
+from flask import Flask, make_response, jsonify, Response, request
+from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+from flask_httpauth import HTTPBasicAuth
 
-@app.route('/create', methods=['POST'])
-def create_emp():
-    try:        
-        _json = request.json
-        _name = _json['name']
-        _email = _json['email']
-        _phone = _json['phone']
-        _address = _json['address']	
-        if _name and _email and _phone and _address and request.method == 'POST':
-            conn = mysql.connect()
-            cursor = conn.cursor(pymysql.cursors.DictCursor)		
-            sqlQuery = "INSERT INTO emp(name, email, phone, address) VALUES(%s, %s, %s, %s)"
-            bindData = (_name, _email, _phone, _address)            
-            cursor.execute(sqlQuery, bindData)
-            conn.commit()
-            respone = jsonify('Employee added successfully!')
-            respone.status_code = 200
-            return respone
-        else:
-            return showMessage()
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close()          
-     
-@app.route('/emp')
-def emp():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT id, name, email, phone, address FROM emp")
-        empRows = cursor.fetchall()
-        respone = jsonify(empRows)
-        respone.status_code = 200
-        return respone
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close()  
+app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-@app.route('/emp/')
-def emp_details(emp_id):
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT id, name, email, phone, address FROM emp WHERE id =%s", emp_id)
-        empRow = cursor.fetchone()
-        respone = jsonify(empRow)
-        respone.status_code = 200
-        return respone
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close() 
+USER_DATA = {"Login": "Login"}
 
-@app.route('/update', methods=['PUT'])
-def update_emp():
-    try:
-        _json = request.json
-        _id = _json['id']
-        _name = _json['name']
-        _email = _json['email']
-        _phone = _json['phone']
-        _address = _json['address']
-        if _name and _email and _phone and _address and _id and request.method == 'PUT':			
-            sqlQuery = "UPDATE emp SET name=%s, email=%s, phone=%s, address=%s WHERE id=%s"
-            bindData = (_name, _email, _phone, _address, _id,)
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sqlQuery, bindData)
-            conn.commit()
-            respone = jsonify('Employee updated successfully!')
-            respone.status_code = 200
-            return respone
-        else:
-            return showMessage()
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close() 
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
 
-@app.route('/delete/', methods=['DELETE'])
-def delete_emp(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute("DELETE FROM emp WHERE id =%s", (id,))
-		conn.commit()
-		respone = jsonify('Employee deleted successfully!')
-		respone.status_code = 200
-		return respone
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PASSWORD'] = 'apzx0dfd6'
+app.config['MYSQL_DB'] = 'badmiton_reservation'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+@app.route('/')
+def hello_world():
+    return "<p>This is my list!</p>"
+
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    return data
+
+@app.route("/players", methods=["GET"])
+@auth.login_required
+def get_players():
+    data = data_fetch("""SELECT * FROM players""")
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/players/<int:id>", methods=["GET"])
+@auth.login_required
+def get_players_by_id(id):
+    data = data_fetch("""SELECT * FROM players WHERE players_id = {}""".format(id))
+    myResponse = make_response(jsonify(data))
+    return myResponse
+    
+
+@app.route("/players", methods=['POST'])
+@auth.login_required
+def add_player():
+    cur = mysql.connection.cursor()
+    json = request.get_json(force=True)
+    players_name = json["players_name"]
+    players_email = json["players_email"]
+    players_contact = json["players_contact"]
+    cur.execute(
+        """ INSERT INTO players (players_name, players_email, players_contact) VALUE (%s, %s, %s)""", (players_name, players_email, players_contact),
+    )
+    mysql.connection.commit()
+    _response = jsonify("player added successfully!")
+    _response.status_code = 200
+    cur.close()
+    return _response
+
+@app.route("/players/<int:id>", methods=["PUT"])
+@auth.login_required
+def update_song_by_id(id):
+    cur = mysql.connection.cursor()
+    json = request.get_json(force=True)
+    players_name = json["players_name"]
+    players_email = json["players_email"]
+    players_contact = json["players_contact"]
+    cur.execute(""" UPDATE players SET players_name = %s, players_email = %s, players_contact = %s WHERE players_id = %s""", (players_name, players_email, players_contact, id))
+    mysql.connection.commit()
+    _response = jsonify("Player updated successfully!")
+    _response.status_code = 200
+    cur.close()
+    return _response
         
-       
+        
+        
+
+@app.route("/players/<int:id>", methods=["DELETE"])
+@auth.login_required
+def delete_song(id):
+    cur = mysql.connection.cursor()
+    cur.execute(""" DELETE FROM players WHERE players_id = %s""", (id,))
+    mysql.connection.commit()
+    cur.close()
+    return make_response(jsonify({"message": "player deleted successfully"}), 200)
+    
+
+
 @app.errorhandler(404)
 def showMessage(error=None):
     message = {
@@ -115,6 +102,6 @@ def showMessage(error=None):
     respone = jsonify(message)
     respone.status_code = 404
     return respone
-        
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
